8b2f1e84d01d16ba3b6a42ba0375e40b7d973572
asukas00 <jessere2025@gmail.com>
2025-10-30 07:17:24 -0700

    Update app.py

--- app.py
+++ app.py
@@ -26,14 +26,46 @@ Juice.buy({"title": "Dull_Blade","atk": 10})
 print(Juice.__dict__) """
 
 class Pet:
-    def __init__(self, name, happiness):
+    def __init__(self, name, happiness, hygiene, thrist, hunger):
         self.name = name
         self.happiness = happiness
+        self.hygiene = hygiene
+        self.thirst = thrist
+        self.hunger = hunger
     def play(self, happy):
-        self.happiness += happy
-        print(f"{self.name} is playing catch")
-        print(f"{self.name}'s increased to {self.happiness}")
-Juice = Pet("Juice", 40)
-Juice.play 
-print(Juice.__dict__)
+        if happy > 80:
+            print(f"{self.name} is now happy")
+        else:
+            self.happiness += happy
+            print(f"{self.name} is playing catch")
+            print(f"{self.name}'s increased to {self.happiness}")
+    def bath(self, hygiene):
+        if hygiene > 80:
+            print(f"{self.name} is now clean")
+        else: 
+            print(f"{self.name} took a bath")
+            hygiene += 10
+            print(f"{self.name}'s hygiene increased to {hygiene}")
+    def drink(self, thirst):
+        if thirst > 80:
+            print(f"{self.name} is now hydrated")
+        else: 
+            print(f"{self.name} drank some water")
+            thirst += 10
+            print(f"{self.name}'s hydration increased to {thirst}")
+    def eat(self, hunger):
+        if hunger > 80:
+            print(f"{self.name} is now realy full")
+        else: 
+            print(f"{self.name} ate some food")
+            hunger += 20
+            print(f"{self.name}'s hunger increased to {hunger}")
+Yourname = input("what is your name?:")
+Petname = input("what is your pet's name?:")
+Pname = Pet("{Petname}", 50, 50, 50, 50)
+
+while True:
+    x = input("what do u want to do with your pet?: play, bath, drink or eat.(p/b/d/e)")
+    if x == ('play') or x == ('p'):
+        Pname.play(10)
     
