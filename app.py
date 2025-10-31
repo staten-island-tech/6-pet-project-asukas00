bf2930c825f651cacf3d7dda035760ff01246828
asukas00 <jessere2025@gmail.com>
2025-10-31 07:17:15 -0700

    A

--- app.py
+++ app.py
@@ -26,46 +26,58 @@ Juice.buy({"title": "Dull_Blade","atk": 10})
 print(Juice.__dict__) """
 
 class Pet:
-    def __init__(self, name, happiness, hygiene, thrist, hunger):
+    def __init__(self, name, happiness, hygiene, thrist, hunger, energy):
         self.name = name
         self.happiness = happiness
         self.hygiene = hygiene
         self.thirst = thrist
         self.hunger = hunger
-    def play(self, happy):
-        if happy > 80:
-            print(f"{self.name} is now happy")
-        else:
-            self.happiness += happy
-            print(f"{self.name} is playing catch")
-            print(f"{self.name}'s increased to {self.happiness}")
+        self.energy = energy
+        energy = 5
+    def play(self, happy, energy):
+        if ('play') == x or ('p') == x:
+            if happy > 80:
+                    print(f"{self.name} is now happy")
+                    energy -= 1
+            elif energy > 0 and happy < 80:
+                self.happiness += happy
+                energy -= 1
+                print(f"{self.name} is playing catch")
+                print(f"{self.name}'s increased to {self.happiness}")
+                print(f"{Pname} now has only {energy} more energy")
+            else:
+                print(f"{Pname} is too tired to play. ")
     def bath(self, hygiene):
-        if hygiene > 80:
-            print(f"{self.name} is now clean")
-        else: 
-            print(f"{self.name} took a bath")
-            hygiene += 10
-            print(f"{self.name}'s hygiene increased to {hygiene}")
+        if ('bath') == x or ('b') == x:
+            if hygiene > 80:
+                energy -= 1
+                print(f"{self.name} is now clean")
+            else: 
+                print(f"{self.name} took a bath")
+                hygiene += 10
+                print(f"{self.name}'s hygiene increased to {hygiene}")
+                energy -= 1
     def drink(self, thirst):
-        if thirst > 80:
-            print(f"{self.name} is now hydrated")
-        else: 
-            print(f"{self.name} drank some water")
-            thirst += 10
-            print(f"{self.name}'s hydration increased to {thirst}")
+        if ('drink') == x or ('d') == x:
+            if thirst > 80:
+                print(f"{self.name} is now hydrated")
+                energy -= 1
+            else: 
+                print(f"{self.name} drank some water")
+                thirst += 10
+                print(f"{self.name}'s hydration increased to {thirst}")
+                energy -= 1
     def eat(self, hunger):
         if hunger > 80:
             print(f"{self.name} is now realy full")
+            energy += 1
         else: 
             print(f"{self.name} ate some food")
             hunger += 20
             print(f"{self.name}'s hunger increased to {hunger}")
+            energy += 1
+
 Yourname = input("what is your name?:")
 Petname = input("what is your pet's name?:")
-Pname = Pet("{Petname}", 50, 50, 50, 50)
-
-while True:
-    x = input("what do u want to do with your pet?: play, bath, drink or eat.(p/b/d/e)")
-    if x == ('play') or x == ('p'):
-        Pname.play(10)
-    
+Pname = Pet(f"{Petname}", 50, 50, 50, 50, 5)
+x = input("What do you want to do with your pet?(play,bath,eat,drink)")
